c98d922df6498a8e21b2954506cdd779c83b58d5
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-12 15:52:29 -0400

    update refresh working directories script

--- refresh_wds.sh
+++ refresh_wds.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/bash
 
-for d in $(cat "$HOME/.j8_scripts/working_directories" | sed 's/ /\\\s/') ; do 
-    test=$(echo $d | sed 's/\\s/ /')
+# Loop through all of the saved working directory logs
+# constructed from 'dirs > wdirs/...' 
+# parse each entry and push to current shell's dirs.
+# Note: You must alias this to run it in the current shell
+for d in $(cat "$HOME/.j8_scripts/wdirs/*" | uniq | sed "s/ /\\\s/") ; do 
+    test=$(echo $d | sed "s/\\s/ /")
     # set -xv
     test=$(echo $test | sed "s:~:$HOME:")
-    pushd "$test"
+    pushd "$test" > /dev/null
     # set +xv
 done
