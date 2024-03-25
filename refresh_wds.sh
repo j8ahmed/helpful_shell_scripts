65f034227d5cd38444e366f25df513f6c7256365
Jamal Ahmed <j8ahmed@gmail.com>
2024-03-25 12:25:12 -0400

    update script to work in current shell using evals

--- refresh_wds.sh
+++ refresh_wds.sh
@@ -4,14 +4,28 @@
 # constructed from 'dirs > wdirs/...' 
 # parse each entry and push to current shell's dirs.
 # Note: You must alias this to run it in the current shell
-wd="$HOME/.j8_scripts/wdirs/"
-dirs=$(for file in $(ls "$wd") ; do cat "$wd/$file" ; done | sort | uniq | sed "s/ /\\\s/")
+test=$(eval echo $HOME/.j8_scripts/wdirs/)
+echo $test
+# dirs=$(for file in $(ls "$wd") ; do cat "$wd/$file" ; done | sort | uniq | sed "s/ /\\\s/")
+# 
+# for d in $dirs ; do
+#     test=$(echo $d | sed "s/\\s/ /")
+#     # set -xv
+#     test=$(echo $test | sed "s:~:$HOME:")
+#     # echo $test
+#     pushd "$test" > /dev/null
+#     # set +xv
+# done
 
-for d in $dirs ; do
-    test=$(echo $d | sed "s/\\s/ /")
-    # set -xv
-    test=$(echo $test | sed "s:~:$HOME:")
-    echo $test
-    # pushd "$test" > /dev/null
-    # set +xv
-done
+# Current version in bashrc aliases
+# load working directories from current shell
+alias loaddirs='
+    for d in $(for file in $(ls "$(eval echo $HOME/.j8_scripts/wdirs/)" ) ; do cat $file ; done | sort | uniq | sed "s/ /\\\s/") ; do 
+        test=$(echo $d | sed "s/\\s/ /")
+        # set -xv
+        test=$(echo $test | sed "s:~:$HOME:")
+        pushd "$test" > /dev/null
+        # set +xv
+    done
+'
+
